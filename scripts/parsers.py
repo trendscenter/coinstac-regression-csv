55a7aaaed64d11f34ed0e4cda42643414cb72221
Fatemeh Mohebbi <mohebbi.fateme@gmail.com>
2020-06-18 22:42:04 -0400

    new changes 06-18

--- scripts/parsers.py
+++ scripts/parsers.py
@@ -18,23 +18,25 @@ def csv_parser(args):
     X_info = input_list["covariates"]
     Y_info = input_list["data"]
 
-    X_file = open(os.path.join(args["state"]["baseDirectory"], X_info[0][0]))
-    Y_file = open(os.path.join(args["state"]["baseDirectory"], Y_info[0][0]))
+    X_data = X_info[0]
+    Y_data = Y_info[0]
     X_labels = X_info[1]
     Y_labels = Y_info[1]
+    X_data = pd.DataFrame.from_records(X_data)
+    Y_data = pd.DataFrame.from_records(Y_data)
+    X_data.columns = X_data.iloc[0]
+    Y_data.columns = Y_data.iloc[0]
+    X_data = X_data.drop(X_data.index[0])
+    Y_data = Y_data.drop(Y_data.index[0])
 
-
-    X_ = pd.read_csv(X_file, header = 0)
-    Y_ = pd.read_csv(Y_file, header = 0)
-    X_.set_index(X_.columns[0], inplace = True)
-    Y_.set_index(Y_.columns[0], inplace = True)
-    X_ = X_[X_labels]
-    Y_ = Y_[Y_labels]
+    X_data.set_index(X_data.columns[0], inplace = True)
+    Y_data.set_index(Y_data.columns[0], inplace = True)
+    X_ = X_data[X_labels]
+    Y_ = Y_data[Y_labels]
     X = X_.apply(pd.to_numeric, errors = 'ignore')
     Y = Y_.apply(pd.to_numeric, errors = 'ignore')
 
-    #X_labels = X_info[1]
-    #Y_labels = Y_info[1]
+
     ixs = X.index.intersection(Y.index)
 
     if ixs.empty:
